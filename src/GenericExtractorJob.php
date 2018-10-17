cfe97f475fee5f3d1ce5d6e740f1d05f5ad6dac3
Tomas Votava <tomas.votava@bizztreat.com>
2018-10-17 15:17:15 +0200

    debug level decreased

--- src/GenericExtractorJob.php
+++ src/GenericExtractorJob.php
@@ -555,7 +555,7 @@ class GenericExtractorJob
 						continue;
 					}
 				}
-				var_dump($parameters);
+				#var_dump($parameters);
 			}
 			if ($parameters != NULL) {
 				$this->config->config["params"] = $parameters;
